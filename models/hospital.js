58ca9ff153e9137a160d921b790041b59dbe5325
Rushikesh/1999 <rushi.shinde1999@gmail.com>
2019-09-27 10:06:18 +0530

    Initial commit

---
+++ models/hospital.js
@@ -0,0 +1,57 @@
+var mongoose = require('mongoose');
+
+var hospitalSchema = new mongoose.Schema({
+    _id:Number,
+    name:{
+        required: true,
+        type:String
+    },
+    category:String,
+    address:{
+        required: true,
+        type:String
+    },
+    state:{
+        required: true,
+        type:String
+    },
+    district:{
+        required: true,
+        type:String
+    },
+    pincode:{
+        required: true,
+        type:Number
+    },
+    telephone:{
+        required: true,
+        type:String
+    },
+    emergency:{
+        required: true,
+        type:String
+    },
+    email:{
+        required: true,
+        type:String
+    },
+    website:{
+        type:String
+    },
+    specialities:{
+        type:Array
+    },
+    latitude:{
+        type:String
+    },
+    longitude:{
+        type:String
+    },
+    facilities:[
+        {type:String}
+    ]
+    
+    
+})
+
+module.exports = mongoose.model("Hospital",hospitalSchema);
