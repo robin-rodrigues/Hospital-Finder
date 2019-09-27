8f66ab287d4c6adea087bea9d544549949514f69
Rushikesh/1999 <rushi.shinde1999@gmail.com>
2019-09-27 14:26:48 +0530

    Landing page

--- routes/hospital.js
+++ routes/hospital.js
@@ -1,8 +1,17 @@
 var express = require("express");
 var router = express.Router();
+var Hospital = require("../models/hospital");
+
 
 router.get("/", function(req, res){
-    res.render("hospital");
+    Hospital.find({}, function(err, allHospitals){
+        if(err){
+            console.log(err);
+        }else{
+            console.log(allHospitals);
+            res.render("hospital", {hospitals: allHospitals});
+        }
+    })
 })
 
 module.exports = router;
