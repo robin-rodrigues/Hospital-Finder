7e2698004a64b2586be46a3411762bbcf8f479a3
Rushikesh/1999 <rushi.shinde1999@gmail.com>
2020-02-25 11:35:21 +0530

    Major changes

--- app.js
+++ app.js
@@ -15,68 +15,6 @@ app.use(HospitalRoutes);
 mongoose
 .connect(process.env.CLOUD_MONGODB_PATH ,{useNewUrlParser: true, useUnifiedTopology: true})
 .then(result => {
-  // console.log(data)
-  // // data.records.forEach(async function(d){
-  //  Hospital.insertMany([
-  //    {
-  //      _id: 1,
-  //      name: "Bhaktivedanta",
-  //      category: "Public",
-  //      address: "Thane",
-  //      location: {
-  //       type: "Point",
-  //       coordinates: [
-  //        "72.870265",
-  //        "19.270236"
-      
-  //       ]
-  //     }
-  //   },{
-  //     _id: 2,
-  //      name: "Breach candy",
-  //      category: "Private",
-  //      address: "Mumbai",
-  //      location: {
-  //       type: "Point",
-  //       coordinates: [
-  //         "72.804416",
-  //         "18.972479"
-      
-  //       ]
-  //     }
-  //     },{
-  //       _id: 3,
-  //       name: "Sahyog Hospital",
-  //       category: "Private",
-  //       address: "Patna",
-  //       location: {
-  //        type: "Point",
-  //        coordinates: [
-  //          "85.108359",
-  //          "25.628991" 
-  //        ]
-  //      }
-  //     }
-      
-  //      //  state: d.state,
-  //     //  district: d.district,
-  //     //  pincode: d._pincode,
-  //     //  telephone: d.telephone,
-  //     //  emergency: d.emergencynum,
-  //     //  email: d.hospitalprimaryemailid,
-  //     //  website: d.website,
-  //     //  specialties:d.specialties,
-  //     // latitude: d._googlemapcorridinate_lati,
-  //     // longitude: d._googlemapcorridinate_longi,
-  //     //  facilities: d.facilities
-     
-    // ])
-    // Hospital.listIndexes().then(indexes => {
-    //   console.log(indexes);
-    // })
-    // .catch(err => {
-    //   console.log(err);
-    // })
   app.listen(3000,function(){
     console.log(" Server has started");
   })
--- routes/hospital.js
+++ routes/hospital.js
@@ -1,44 +1,85 @@
 var express = require("express");
 var router = express.Router();
 var Hospital = require("../models/hospital");
-// let filter = {}
 
 router.get("/", function(req, res){
-     console.log(req.qeury)
     let coordinates = {}
-    let filter ={}
-            console.log(req.query)
-            coordinates.latitude = req.query.latitude ? req.query.latitude : 0,
-            coordinates.longitude = req.query.longitude ? req.query.longitude : 0
+    let filter = {
+        lat: req.query.latitude ? req.query.latitude : 0,
+        long: req.query.longitude ? req.query.longitude : 0,
+        name: req.query.Name ? req.query.Name : 0,
+        Place: req.query.place ? req.query.place: "Near me"
+    } 
+        
+        coordinates.latitude = req.query.latitude ? req.query.latitude : 0,
+        coordinates.longitude = req.query.longitude ? req.query.longitude : 0
+           if(req.query.place){
+            Hospital.find({
+                place: req.query.place
+            }, function(err, allHospitals){
+                if(err){
+                    console.log(err);
+                }else{
+                    if(req.query.Name){
+                        allHospitals = allHospitals.filter(hospital=> hospital.name == req.query.Name);
+                    }
+                    if(req.query.bloodbank){
+                        allHospitals = allHospitals.filter(hospital=> hospital.bloodbank == req.query.bloodbank);
+                    }
+                        if(req.query.public && req.query.private){
+                            allHospitals = allHospitals;
+                        }
+                        else {
+                            if(req.query.public){
+                            allHospitals = allHospitals.filter(hospital => hospital.category == req.query.public);
+                            }
+                            if(req.query.private){
+                                allHospitals = allHospitals.filter(hospital => hospital.category == req.query.private);
+                            }
+                        }
+                     res.render("hospital", {hospitals: allHospitals, filter:filter});   
+                } 
+            })
+           }
            
-            Hospital.find({ 
-                    location:{ $near : { $geometry: { type: "Point",  coordinates: [ Number(coordinates.longitude),Number(coordinates.latitude)] },$maxDistance: 500000} }
-                }, function(err, allHospitals){
+           else{
+            if(req.query.Name){
+        Hospital.find({
+                    name: req.query.Name
+                },function(err, allHospitals) {
                     if(err){
                         console.log(err);
                     }else{
-                        if(req.query.Name){
-                            allHospitals = allHospitals.filter(hospital=> hospital.name == req.query.Name);
-                        }
-                        if(req.query.bloodbank){
-                            allHospitals = allHospitals.filter(hospital=> hospital.bloodbank == req.query.bloodbank);
-                        }
-                        console.log(allHospitals);
-                            if(req.query.public && req.query.private){
-                                allHospitals = allHospitals;
-                            }
-                            else {
-                                if(req.query.public){
-                                allHospitals = allHospitals.filter(hospital => hospital.category == req.query.public);
-                                }
-                                if(req.query.private){
-                                    allHospitals = allHospitals.filter(hospital => hospital.category == req.query.private);
-                                }
-                            }
-                         res.render("hospital", {hospitals: allHospitals});        
+                        res.render('hospital',{hospitals: allHospitals, filter: filter})
                     }
                 })
+            }
+        Hospital.find({ 
+                location:{ $near : { $geometry: { type: "Point",  coordinates: [ Number(coordinates.longitude),Number(coordinates.latitude)] },$maxDistance: 500000} },
+        }, function(err, allHospitals){
+                if(err){
+                    console.log(err);
+                }else{
+                    
+                    if(req.query.bloodbank){
+                        allHospitals = allHospitals.filter(hospital=> hospital.bloodbank == req.query.bloodbank);
+                    }
+                        if(req.query.public && req.query.private){
+                            allHospitals = allHospitals;
+                        }
+                        else {
+                            if(req.query.public){
+                            allHospitals = allHospitals.filter(hospital => hospital.category == req.query.public);
+                            }
+                            if(req.query.private){
+                                allHospitals = allHospitals.filter(hospital => hospital.category == req.query.private);
+                            }
+                        }
+                     res.render("hospital", {hospitals: allHospitals, filter: filter});        
+                }
             })
+}
+})
 
 router.get("/map",function(req,res){
     Hospital.find({},function(err, allHospitals){
